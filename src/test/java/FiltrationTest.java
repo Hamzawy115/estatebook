6ae3f069de2a31a5fb3f324b165aa501958310d3
Hamzawy115 <mohamed.mostafa@theaddressholding.com>
2023-11-21 10:45:34 +0200

    true case

--- src/test/java/FiltrationTest.java
+++ src/test/java/FiltrationTest.java
@@ -36,23 +36,22 @@ public class FiltrationTest extends TestBase {
         }
 
     }*/
-/*    @Test( priority = 2)
-    public void testOfTypeFilter2() {
+/* @Test(priority = 2)
+ public void testOfTypeFilter2() {
 
-        var filterByType = homePage.clickDiscoverBtn()
-                .clickTypeExpend()
-                .pickServicedApartment()
-                .pickTriplex()
-                .clickApplyTypeButton();
+     var filterByType = homePage.clickDiscoverBtn()
+             .clickTypeExpend()
+             .pickServicedApartment()
+             .pickTriplex()
+             .clickApplyTypeButton();
 
-        Assert.assertTrue(filterByType.checkBuildingType("Serviced Apartment", "Triplex"));
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-
-    }*/
+     Assert.assertTrue(filterByType.checkBuildingType("Serviced Apartment", "Triplex"));
+     try {
+         Thread.sleep(5000);
+     } catch (InterruptedException e) {
+         throw new RuntimeException(e);
+     }
+ }*/
 
     @Test(priority = 1)
     public void testOfPriceFilter() {
@@ -61,8 +60,6 @@ public class FiltrationTest extends TestBase {
                 .clickPriceExpend()
                 .enterMinAndMaxPrice("5000000", "20000000")
                 .clickApplyPriceButton();
-
-
         Assert.assertTrue(filterByPrice.checkPriceOfBuilding("5000000", "20000000"));
         try {
             Thread.sleep(5000);
